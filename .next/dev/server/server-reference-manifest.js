a50e39476cb722aca6a46b929fa975697d10884f
Kalai <spydybat@email.com>
2026-04-10 20:00:26 +0530

    clean repo - removed .next

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"ZP59N2HAhC6XOmH0sZ6GwL9zMDpBE+ARR/41iQip/Oo=\"\n}"
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"/8OmCgmLa0aqLGOtQv7RqEkdjxIqqiHHXGJjfI1BJM4=\"\n}"
